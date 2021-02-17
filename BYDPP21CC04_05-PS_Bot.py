3aa35cc60fa6ca3109e99ac6e802aceb4b81c539
Alexandros Xafopoulos <42879364+lxndroc@users.noreply.github.com>
2021-02-17 10:33:00 +0000

    Update and rename BYDPP21CC04-PS_Bot.py to BYDPP21CC04_05-PS_Bot.py

--- BYDPP21CC04_05-PS_Bot.py
+++ BYDPP21CC04_05-PS_Bot.py
@@ -1,8 +1,8 @@
 '''
 + Build Your Dream Python Project (BYDPP) +
 Team Invite: https://dsc.gg/python_team
-BYDPP 2021 Coaching Call #04 (BYDPP21CC04)
-12 Feb 2021, 8pm GMT
+BYDPP 2021 Coaching Call #05 (BYDPP21CC05)
+15 Feb 2021, 8pm GMT
 Host: @Bertan
 Coach: @alexandros
 
@@ -11,8 +11,8 @@ Write a bot trying to order a product, which is not in stock and limited.
 When the product becomes available, the bot is ordering it automatically.
 This is useful for limited stuff, e.g. PS5, Xbox, shoes, Graphics cards.
 If the product is unavailable, the bot presses F5 to refresh the page and checks again.
-When the product is available, the bot orders it by clicking the button
-that adds the product to the cart.
+When the product is available, the bot is ordering it by clicking the button
+that adds the product to the cart, auto-filling the requested details, and performing checkout.
 
 Installations Required
 > selenium package
@@ -20,8 +20,8 @@ Installations Required
 > Selenium WebDriver for Chrome
 > Google Chrome
 
-Python Code - Initial Effort
-As below, being non-optimised, as in the call.
+Python Code - 2nd Effort
+As below, being non-optimised, as in the call with minor edits.
 '''
 
 from selenium import webdriver
@@ -31,26 +31,44 @@ from time import sleep
 class PS_Bot:
 
     def __init__(self):
+        self.mail = input("Enter your email address: ")
         # Test order is a random link that I chose from the website which is in stock. The reason is checking the source codes of addtocartButton
         urlTest = 'https://www.smythstoys.com/uk/en-gb/toys/fashion-and-dolls/soft-toys/brown-bear-plush-100cm/p/168724'
 
         # The actual link is a PlayStation 5 which is not in stock.
         url = 'https://www.smythstoys.com/uk/en-gb/video-games-and-tablets/playstation-5/playstation-5-consoles/playstation-5-digital-edition-console/p/191430'
+
         # We have to install webdriver for Chrome and I just put the path of the driver.
         self.driver = webdriver.Chrome(executable_path="C:/Windows/chromedriver.exe")
 
         # This code is opening the url
         self.driver.get(urlTest)
-        self.availability_check()
+        self.availabilitycheck()
 
-    def availability_check(self):
+    def availabilitycheck(self):
         # I defined button as Add to Cart button
         while True:
             try:
-                button = self.driver.find_element_by_xpath('/html/body/div[7]/section/div/div/div[2]/div[1]/div[5]/div/div/div/div[2]/form/button')
+                cookies = self.driver.find_element_by_xpath('//*[@id="cookieLoad"]/div/div/div[1]/div[4]/div[1]/button')
+                cookies.click()
+                sleep(2)
+                button = self.driver.find_element_by_xpath('//*[@id="addToCartButton"]')
                 button.click()
+                sleep(1)
+                self.driver.get('https://www.smythstoys.com/uk/en-gb/cart')
+                sleep(1)
+                checkout = self.driver.find_element_by_xpath('//*[@id="checkoutOnCart"]')
+                checkout.click()
+                sleep(2)
+                email = self.driver.find_element_by_xpath('//*[@id="guest.email"]')
+                email.send_keys(self.mail)
+                email_repeat = self.driver.find_element_by_xpath('//*[@id="guest.confirmEmail"]')
+                email_repeat.send_keys(self.mail)
+                checkout_order = self.driver.find_element_by_xpath('//*[@id="confirmEmailGuestForm"]/div[1]/div[2]/div/div[2]/div/button')
+                checkout_order.click()
+
+                input()
                 break
-            #if the button is unavailable, refresh the page and check it again.
             except:
                 print("Button not found")
                 sleep(1)
